c3ca73da4e0b8bd40c338634e856feae31f621bb
Trojanzaro <anastasiszaro@gmail.com>
2025-01-13 19:46:58 +0200

    Added coordinate transformation from polar to cartesian, changed electronic values for 9.0 v battery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import sys
 import pigpio
+import math  
 
 import sonar_trigger_echo
 import motors_ctr
@@ -126,21 +127,21 @@ try:
 
         # START MEASURING DISTANCES
         # START WITH A 90° rotation
-        motors.CLOCK_P(1.60) # Δt ~= 1.45 sec =~ 90° 
+        motors.CLOCK_P(1.30) # Δt ~= 1.45 sec =~ 90° | ! 1.30 Δt with 9.0 V battery and 170 Duty Cycle
         time.sleep(1) # wait for one second
 
         # array for the distances
         dist = []
 
         # START TAKING DISTANCE MEASURE MENTS AND APPEND THEM IN THE dist ARRAY
-        n = 125 # 45 steps of 2° rotation for 90° rotation | 116 steps ~180° rotation
+        n = 73 # 45 steps of 2° rotation for 90° rotation | 116 steps ~180° rotation | ! 73 STEPS FOR 9.0 V battery and 170 Duty Cycle
         while n > 0:
 
             # GET A READING AND ADD IT TO THE DISTANCE ARRAY
-            r = sonar.read()
+            r = sonar.read() # read duration
             r = (r/2) / 29.1 # normalize duration to cm
             print("D: {}".format(r))
-            dist.append(r)
+            dist.append(r) # add to distances array
 
             # TURN ROVER IN STEPS
             motors.CLOCK(0.05) # Δt = 5ms =~ 2°
@@ -155,20 +156,33 @@ try:
         plt.grid(True)
 
         # Generating the X and Y axis data point
-        theta = np.deg2rad(np.arange(180, 0, -1.44)) # magic number necesary for 116 steps
+        # Generating the θ range 180° - 0° 
+        theta = np.deg2rad(np.arange(180, 0, -2.46575342466)) # magic number necesary for 116 steps | ! 73 steps
 
         # plotting the polar coordinates on the system
         plt.polar(theta, dist, marker='o')
-
-        # Setting the axis limit
-        #ax.set_ylim(0,10)
-
         plt.savefig("map.png")
 
-        print(dist)
+        # transformation from polar to cartesian coordinates
+        fig = plt.figure(dpi=200)
+        ax = fig.add_subplot()
+        plt.grid(True)
 
+        # where the coordinates will be stored
+        x1 = []
+        y1 = []
 
+        # for the entire range transform from [r, θ] to [x, y]
+        for i in range(0, 73):
+            x = dist[i]*math.cos(theta[i]) # x = r1 cos(θ1)
+            x1.append(x)
 
+            y = dist[i]*math.sin(theta[i]) # y = r1 sin(θ1) 
+            y1.append(y)
+
+        # save plot 
+        plt.plot(x1, y1, marker='o')
+        plt.savefig("map2.png")
 
 
 except KeyboardInterrupt:
