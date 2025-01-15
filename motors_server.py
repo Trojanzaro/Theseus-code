5061cb4f7595e59688be3f519afc8e28725056d9
Trojanzaro <anastasiszaro@gmail.com>
2025-01-15 19:51:54 +0200

    fixed problem and added more controls for motor server

--- motors_server.py
+++ motors_server.py
@@ -1,5 +1,5 @@
 import pigpio
-from flask import Flask
+from flask import Flask, g
 import motors_ctr
 
 # setup PIGPIO
@@ -10,22 +10,25 @@ pi1 = pigpio.pi()       # pi1 accesses the local Pi's GPIO
 def cb_stop_bt(gpio, level, tick):
     print(gpio, level, tick)
     print("STOPPING APPLICATION")
-    motors.HALT()
+    get_motors_instance().HALT()
     pi1.stop()
     exit()
 # interuopt GPIO forstopping by callback cb_stop_bt 
 cb1 = pi1.callback(4, pigpio.EITHER_EDGE, cb_stop_bt)
 
 # Initialize the motors GPIOs 
-motors = motors_ctr.Motors(pi1, 17, 27, 22, 10, 9, 11, 5, 6)
+# FLASK GLOBAL OBJECT REQUIRED! in order to store motors_instance in the global namespace ('g')
+def get_motors_instance():
+    if 'mot_ins' not in g:
+        g.mot_ins = motors_ctr.Motors(pi1, 17, 27, 22, 10, 9, 11, 5, 6)
+    return g.mot_ins
 
 ####
-def create_motors_server(motors_instance):
+def create_motors_server():
         app = Flask(__name__)
-        app.config['MOTORS'] = motors_instance
         return app
 
-app = create_motors_server(motors)
+app = create_motors_server()
 
 @app.route("/")
 def index(): 
@@ -33,33 +36,37 @@ def index():
 
 @app.route('/north')
 def north():
-    app.config['MOTORS'].NORTH()
-    print("YO I'M HERE!!!")
+    get_motors_instance().NORTH()
     return "<h1>hello</h1>"
 
 @app.route('/south')
 def south():
-    app.config['MOTORS'].SOUTH()
-    print("YO I'M HERE!!!")
+    get_motors_instance().SOUTH()
     return "<h1>hello</h1>"
-
 
 @app.route('/left')
 def left():
-    app.config['MOTORS'].STRAFE_LEFT()
-    print("YO I'M HERE!!!")
+    get_motors_instance().STRAFE_LEFT()
     return "<h1>hello</h1>"
 
 @app.route('/right')
 def right():
-    app.config['MOTORS'].STRAFE_RIGHT()
-    print("YO I'M HERE!!!")
+    get_motors_instance().STRAFE_RIGHT()
+    return "<h1>hello</h1>"
+
+@app.route('/clock')
+def clock():
+    get_motors_instance().CLOCK()
+    return "<h1>hello</h1>"
+
+@app.route('/clock_p')
+def clock_p():
+    get_motors_instance().CLOCK_P()
     return "<h1>hello</h1>"
 
 @app.route('/halt')
 def halt():
-    app.config['MOTORS'].HALT()
-    print("YO I'M HERE!!!")
+    get_motors_instance().HALT()
     return "<h1>hello</h1>"
 ####
 
