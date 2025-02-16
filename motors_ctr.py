8ac8e4a14be0760aed9baa4ef7c91055b4246298
Trojanzaro <anastasiszaro@gmail.com>
2025-02-16 20:59:51 +0200

    Added encoder handler code and calculated ppr and angles for NL and SL motors

--- motors_ctr.py
+++ motors_ctr.py
@@ -1,15 +1,38 @@
 import pigpio
 import time
 import numpy as np
+from gpiozero import RotaryEncoder
 
 class Motors:
     """The control class for Theseus motors"""
 
     """Motor PWM duty dycle """
     pwm_dc = 127
-    pos =0
 
-    def __init__(self, pi1, NLL, NLH, NRL, NRH, SLL, SLH, SRL, SRH, NLA, NLB):
+    """Wheel position counter per pulse"""
+    wheel_pulse_count = 0
+
+    """ Motor encoder output pulses per 360 degree revolution (measured manually) ppr or Pulses Per Revolution of the encoder """
+    ENC_PPR = 543 # calculated to be 543 for 360 degrees of rotation
+
+    """Encoder object"""
+    encoderNL = None
+    encoderNR = None
+    encoderSL = None
+    encoderSR = None
+
+    # Variable for RPM measuerment
+    rpm_right = 0.0
+    
+    # Variable for angular velocity measurement
+    ang_velocity_right = 0.0
+    ang_velocity_right_deg = 0.0
+    
+    # CONSTANTS FOR CALCULATIONS
+    rpm_to_radians = 0.10471975512
+    rad_to_deg = 57.29578
+
+    def __init__(self, pi1, NLL, NLH, NRL, NRH, SLL, SLH, SRL, SRH, NLA, NLB, NRA=None, NRB=None, SLA=None, SLB=None, SRA=None, SRB=None):
         """The pigpio object for controlling the GPIOS"""
         self.pi = pi1
 
@@ -34,10 +57,6 @@ class Motors:
         """North Left Motor Phase"""
         self.NLA = NLA
         self.NLB = NLB
-        self.levA = 0
-        self.levB = 0
-
-        self.lastGpio = None
 
         # setup all pins
         # MOTOR Vs/GND/PWM
@@ -53,34 +72,11 @@ class Motors:
         self.pi.set_mode(SRH, pigpio.OUTPUT)
 
         #MOTORS ENCODERS PHASES
-        self.pi.set_mode(NLA, pigpio.INPUT)
-        self.pi.set_mode(NLB, pigpio.INPUT)
-        self.pi.set_pull_up_down(NLA, pigpio.PUD_UP)
-        self.pi.set_pull_up_down(NLB, pigpio.PUD_UP)
-        self.cbA = self.pi.callback(NLA, pigpio.EITHER_EDGE, self._pulseA)
-        self.cbB = self.pi.callback(NLB, pigpio.EITHER_EDGE, self._pulseA)
-
-    def callback(self, way):
-        self.pos += way
-        print("pos={}".format(self.pos))
-
-    def _pulseA(self, gpio, level, tick):
-        #print(gpio, level, tick)
-        if gpio == self.NLA:
-            self.levA = level
-        else:
-            self.levB = level
-
-        if gpio != self.lastGpio: # debounce
-            self.lastGpio = gpio
-
-            if gpio == self.NLA and level == 1:
-                if self.levB == 1:
-                    self.callback(1)
-            elif gpio == self.NLB and level == 1:
-                if self.levA == 1:
-                    self.callback(-1)
-
+        #MOTOR ENCODERS 
+        self.encoderNL = RotaryEncoder(NLA, NLB, max_steps=0)
+        #self.encoderNR = RotaryEncoder(NRA, NRB, max_steps=0)
+        #self.encoderSL = RotaryEncoder(SLA, SLB, max_steps=0)
+        #self.encoderSR = RotaryEncoder(SRA, SRB, max_steps=0)
 
     # change DutyCycle
     def change_dc(self, dc):
@@ -92,8 +88,7 @@ class Motors:
     # return PWM for rad/s
     def rads2pwm(self, rads=None):
         #pwm_dc = (rads + 49.91) / 2.096 # ! Magic numbers come from the statistical analysis of the motors PWM to rad/s, linear regresion f(x) = 0.034x - 0.5
-        return np.ceil(np.interp(rads, [0, (np.pi*5)/2], [0, 255])) if rads != None else self.pwm_dc
-
+        return np.ceil(np.interp(rads, [-1.0, 0.0, 1.0], [0, 127, 255])) if rads != None else self.pwm_dc
 
     def HALT(self):
         # North wheels
@@ -416,41 +411,64 @@ class Motors:
             # halt motors
             self.HALT()
 
-    def W(self, dt=None, w1=None, w2=None, w3=None, w4=None):
+    def get_angle(self, i=0):
+        if i == 0:
+            return 360 / self.ENC_PPR * self.encoderNL.steps
+        if i == 1:
+            pass #return 360 / self.ENC_PPR * self.encoderNR.steps
+        if i == 2:
+            pass #return 360 / self.ENC_PPR * self.encoderSL.steps # return the angle the ith wheel is at by calculating (360 / encoders_pulses_per_revolution * encoder_steps)
+        if i == 3:
+            pass #return 360 / self.ENC_PPR * self.encoderSR.steps
+
+
+    def W(self, dth=None, w1=None, w2=None, w3=None, w4=None):
         # North wheels
         if w1 > 0:
             self.pi.set_PWM_dutycycle(self.NLL, self.rads2pwm(w1)) # NL
             self.pi.write(self.NLH, 0)               # -
-        else:
+        elif w1 < 0:
             self.pi.write(self.NLL, 0)               # NL
             self.pi.set_PWM_dutycycle(self.NLH, self.rads2pwm(w1*(-1))) # -
+        else:
+            self.pi.write(self.NLH, 0) # -
+            self.pi.write(self.NLL, 0)  
 
         if w2 > 0:
             self.pi.set_PWM_dutycycle(self.NRL, self.rads2pwm(w2)) # NR
             self.pi.write(self.NRH, 0)               # -
-        else:
+        elif w2 < 0:
             self.pi.write(self.NRL, 0)               # NR
             self.pi.set_PWM_dutycycle(self.NRH, self.rads2pwm(w2*(-1))) # - 
-        
+        else:
+            self.pi.write(self.NRL, 0)
+            self.pi.write(self.NRH, 0) # - 
+
 
         # South wheels
         if w3 > 0:
             self.pi.set_PWM_dutycycle(self.SLL, self.rads2pwm(w3))  # SL
             self.pi.write(self.SLH, 0)
-        else:
+        elif w3 < 0:
             self.pi.write(self.SLL, 0)                # SL
             self.pi.set_PWM_dutycycle(self.SLH, self.rads2pwm(w3*(-1))) # -
+        else:
+            self.pi.write(self.SLL, 0)                # SL
+            self.pi.write(self.SLH, 0) # -
 
         if w4 > 0:
             self.pi.set_PWM_dutycycle(self.SRL, self.rads2pwm(w4))  # SR
             self.pi.write(self.SRH, 0)                # -
-        else:
+        elif w4 < 0:
             self.pi.write(self.SRL, 0)                 # SR
             self.pi.set_PWM_dutycycle(self.SRH, self.rads2pwm(w4*(-1))) # -
+        else:
+            self.pi.write(self.SRL, 0)                 # SR
+            self.pi.write(self.SRH, 0)                # -
 
         # delay for distance (* .5 sec for ~5cm)
-        if dt != None:
-            time.sleep(dt)
+        if dth != None:
+            time.sleep(dth)
 
             # halt motors
             self.HALT()
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import time
 import pigpio 
 import numpy as np
+import csv
 
 import motors_ctr
 import sonar_trigger_echo
@@ -21,7 +22,7 @@ def cb_stop_bt(gpio, level, tick):
 cb1 = pi1.callback(4, pigpio.EITHER_EDGE, cb_stop_bt)
 
 # Initialize the motors GPIOs 
-motors = motors_ctr.Motors(pi1, 17, 27, 22, 10, 9, 11, 5, 6, 16, 12)
+motors = motors_ctr.Motors(pi1, 17, 27, 22, 10, 9, 11, 5, 6, 12, 16)
 
 # setup HC-SR04 ultranosic sensor
 sonar = sonar_trigger_echo.ranger(pi1, 23, 18)
@@ -63,25 +64,84 @@ try:
 
     #T_P = np.multiply(0.33/4, T_P)
     print(T, w1, w2, w3, w4)
-    motors.change_dc(64) 
-    
+    motors.change_dc(64)
+    motors.HALT()
+
+    # Assigning parameter values
+    # Pulses Per Revolution of the encoder, 
+    # calculated by counting the pulses each inerupt the hall effect sensors make 
+    ppr = 543  
+    tstop = 10  # Loop execution duration (s)
+    tsample = 0.02  # Sampling period for code execution (s)
+    tdisp = 0.5  # Sampling period for values display (s)
+
+    anglecurr = 0
+    tprev = 0
+    tcurr = 0
+    tstart = time.perf_counter()
+
+    # Execution loop that displays the current
+    # angular position of the encoder shaft
+    print('Running code for', tstop, 'seconds ...')
+    print('(Turn the encoder.)')
     while True:
-        #motors.W(w1=w1, w2=w2, w3=w3, w4=w4) # ! NEW FUNCTION FOR ALL MOTORs ANGULAR VELOCITY MOVE
-        #motors.W(w1=5*np.pi/2, w2=0, w3=5*np.pi/2, w4=0)
-        
-        motors.NORTH(dt=2)
-        motors.SOUTH(dt=2)
+        # Pausing for `tsample` to give CPU time to process encoder signal
+        time.sleep(tsample)
+        # Getting current time (s)
+        tcurr = time.perf_counter() - tstart
+        # Printing angular position every `tdisp` seconds
+        if (np.floor(tcurr/tdisp) - np.floor(tprev/tdisp)) == 1:
+            print("Angle = {:0.0f} deg".format(motors.get_angle(0)))
+        # Updating previous values
+        tprev = tcurr
 
-        distance1 = (sonar.read()/2) / 29.1
-        #time.sleep(0.5)
-        distance2 = (sonar.read()/2) / 29.1
+    print('Done.')
+    # Releasing GPIO pins
 
-        speed = (distance1 - distance2) / 0.5
-        #print("{}".format(motors.pos))
-        #print("Speed: {}".format(speed))
-        #motors.print_phases()
-        #break
-        
+    # Assigning parameter values
+    T = 2  # Period of sine wave (s)
+    u0 = 1  # Motor output amplitude
+    tstop = 4  # Sine wave duration (s)
+    tsample = 0.01  # Sampling period for code execution (s)
+    # Pre-allocating output arrays
+    t = []
+    theta = []
+
+    # Initializing current time step and starting clock
+    tprev = 0
+    tcurr = 0
+    tstart = time.perf_counter()
+
+    # Running motor sine wave output
+    print('Running code for', tstop, 'seconds ...')
+    while tcurr <= tstop:
+        # Pausing for `tsample` to give CPU time to process encoder signal
+        time.sleep(tsample)
+        # Getting current time (s)
+        tcurr = time.perf_counter() - tstart
+        # Assigning motor sinusoidal output using the current time step
+        motors.W(w1=0, w2=0, w3=u0 * np.sin((2*np.pi/T) * tcurr), w4=0)
+        # Updating output arrays
+        t.append(tcurr)
+        #theta.append(mymotor.get_angle())
+        # Updating previous time value
+        tprev = tcurr
+        with open('eggs.csv', 'a', newline='') as csvfile:
+            spamwriter = csv.DictWriter(csvfile, fieldnames=['sin(x)', 'pos'])
+            spamwriter.writerow({'sin(x)': u0 * np.sin(2*np.pi/T) * tcurr, 'pos': motors.pos})
+
+    print('Done.')
+    # Stopping motor and releasing GPIO pins
+    motors.HALT()
+
+    # Calculating motor angular velocity (rpm)
+    #w = 60/360 * np.gradient(theta, t)
+
+
+    # Plotting results
+    #plot_line([t, t, t[1::]], [theta, w, 1000*np.diff(t)], axes='multi',yname=['Angular Position (deg.)','Angular velocity (rpm)','Sampling Period (ms)'])
+
+    encoder.close()
 
     print("STOPPING!")
     motors.HALT()
