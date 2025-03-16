5b17d755acc70bc4845b000283c9481531247b6a
Trojanzaro <anastasiszaro@gmail.com>
2025-03-16 15:18:40 +0200

    Update motor_controller.ino

--- motor_controller.ino
+++ motor_controller.ino
@@ -1,10 +1,14 @@
 void setup() {
   DDRD |= 0xfc; // Set D2-D7 as outputs
   DDRB |= 0x03; // Set D8-D9 as outputs
+  Serial.begin(9600);
 }
 
+byte pwm = 64;
 byte pwmCounter = 0; // Naturally limited to 0-255
-byte pwms[] = { 10, 20, 40, 80, 10, 20, 40, 80 };
+byte pwms[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+int steps = 0;
 
 void loop() {
   PORTD ^= (-(pwmCounter >= pwms[0]) ^ PORTD) & (1 << 2); // D2
@@ -19,8 +23,24 @@ void loop() {
 
   pwmCounter += 1;
 
-  //TODO:
+  if((PINB & 0b00000100) >> 2) {
+    if((PINB & 0b00001000) >> 3) {
+      steps++;
+    }
+  }
+  if((PINB & 0b00001000) >> 3) {
+    if(!(PINB & 0b00000100) >> 2) {
+      steps--;
+    }
+  }
+  Serial.print((PINB & 0b00000100) >> 2);
+  Serial.print(" ");
+  Serial.print((PINB & 0b00001000) >> 3);
+  Serial.print(" ");
+  Serial.println(steps);
 }
+
+//TODO
 void enc_print() {
   if ((PORTD & 0b00000100) == 0){ 
     if  ((PORTD & 0b00001000) == 0){
