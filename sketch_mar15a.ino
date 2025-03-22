601c2ca382f2c8f31c782afe3debb2555c5fd651
Trojanzaro <anastasiszaro@gmail.com>
2025-03-22 20:35:16 +0200

    Replaced encoder logic, finally works

--- sketch_mar15a.ino
+++ sketch_mar15a.ino
@@ -1,44 +1,39 @@
-byte rotary_state_mapping[] = { 0, 1, 2};
-byte last_state;
 int counter = 0;
+byte last_enca = 0;  // Last state of D11
+byte last_encb = 0;  // Last state of D12
 
 void setup() {
-  DDRB &= (~0x0c); // Set D11-12 as inpus
-  last_state = (PINB & 0x0c) >> 2;
-  //last_state = rotary_state_mapping[last_state];
+  DDRB &= ~(1 << PB3);  // Clear bit 3 of DDRB (D11 as input)
+  DDRB &= ~(1 << PB4);  // Clear bit 4 of DDRB (D12 as input)
+
+  // Optionally enable pull-up resistors for D11 and D12
+  PORTB |= (1 << PB3);  // Enable pull-up resistor on D11 (PORTB3)
+  PORTB |= (1 << PB4);  // Enable pull-up resistor on D12 (PORTB4)
+  Serial.begin(9600);
 }
 
 void loop() {
   // Encoder reading logic
-  byte current_state = (PINB & 0x0c) >> 2;
-  //current_state = rotary_state_mapping[last_state];
+  last_enca = (PINB & (1 << PB3)) != 0;
+  last_encb = (PINB & (1 << PB4)) != 0; 
+  
+  byte enca = (PINB & (1 << PB3)) != 0;
+  byte encb = (PINB & (1 << PB4)) != 0;
 
   // Compare the current and last states to determine direction
-  if(current_state != 2 && last_state != 2){ // There is a sporadic HIGH latch on one of the two phases
-    if(current_state == 0 && last_state == 0) {
+  if(enca) {
+    if(!last_encb) {
+      counter++; // if encoder A is on and B last state is 0 CLOCKWISE
+    } else {
       return;
-      }
-    if(current_state == 2 && last_state == 0){
-      counter++;
-      }
-    if(current_state == 2 && last_state == 2){
-      return;
-      }
-    if(current_state ==2 && last_state == 0){
-      counter--;
-      }
-    if(current_state == 0 && last_state ==2) {
-      return;
-      }
-  }
-  if (Serial.available() > 0) {
-  Serial.print((PINB & 0x0c) >> 2);
-  Serial.print(" ");
-  Serial.print((PINB & 0x0c) >> 2);
-  Serial.print(" ");
-  Serial.println(counter);
-    
     }
-    
-  last_state = (PINB & 0x0c) >> 2;
+  } else if(encb){
+    if(!last_enca) {
+      counter--; // if encoder B is on and A last state is 0 COUNTER-CLOCKWISE
+    } else{
+      return;
+    }
+  }
+  
+  Serial.println(counter);
 }
