408b2111ae3a5f0432c447a03e3560e533bad34a
Trojanzaro <anastasiszaro@gmail.com>
2025-03-26 20:28:19 +0200

    Update sketch_mar15a.ino to include new Rotary.h lib

--- sketch_mar15a.ino
+++ sketch_mar15a.ino
@@ -1,14 +1,16 @@
-//Encoder Variables
-int counter = 0;
-byte last_enca = 0;  // Last state of D11
-byte last_encb = 0;  // Last state of D12
-
+#include <math.h>
+#include "Rotary.h"
 
 //PWM Variables
 byte pwm = 64;
 byte pwmCounter = 0; // Naturally limited to 0-255
-byte pwms[] = { 0, pwm, 0, pwm, 0, pwm, 0, pwm};
 
+byte pwms[] = { 0, pwm, 0, pwm, 0, pwm, 0, pwm}; 
+byte pwms_0[] = { 0, 0, 0, 0, 0, 0, 0, 0}; 
+
+double t = 0;
+
+Rotary enc1 = Rotary(11, 12);
 
 void setup() {
   DDRD |= 0xfc; // Set D2-D7 as outputs
@@ -20,44 +22,45 @@ void setup() {
   // Optionally enable pull-up resistors for D11 and D12
   PORTB |= (1 << PB3);  // Enable pull-up resistor on D11 (PORTB3)
   PORTB |= (1 << PB4);  // Enable pull-up resistor on D12 (PORTB4)
-  //Serial.begin(115200);
+  Serial.begin(230400);
 }
 
 void loop() {
-  // PWM writing logic
-  PORTD ^= (-(pwmCounter >= pwms[0]) ^ PORTD) & (1 << 2); // D2
-  PORTD ^= (-(pwmCounter >= pwms[1]) ^ PORTD) & (1 << 3); // D3
-  PORTD ^= (-(pwmCounter >= pwms[2]) ^ PORTD) & (1 << 4); // D4
-  PORTD ^= (-(pwmCounter >= pwms[3]) ^ PORTD) & (1 << 5); // D5
-  PORTD ^= (-(pwmCounter >= pwms[4]) ^ PORTD) & (1 << 6); // D6
-  PORTD ^= (-(pwmCounter >= pwms[5]) ^ PORTD) & (1 << 7); // D7
+  t += 0.0005;
+  float x = (cos(t)/1)*255;
 
-  PORTB ^= (-(pwmCounter >= pwms[6]) ^ PORTB) & (1 << 0); // D8
-  PORTB ^= (-(pwmCounter >= pwms[7]) ^ PORTB) & (1 << 1); // D9
+  // PWM writing logic
+  byte selected_pwms[] = {0,x,0,x,0,x,0,x};
+  if(x < 0){
+    selected_pwms[0] = abs(x);
+    selected_pwms[1]= 0;
+    selected_pwms[2] = abs(x);
+    selected_pwms[3] = 0;
+    selected_pwms[4]= abs(x);
+    selected_pwms[5]= 0;
+    selected_pwms[6]= abs(x);
+    selected_pwms[7]= 0;
+  }
+
+  PORTD ^= (-(pwmCounter >= selected_pwms[0]) ^ PORTD) & (1 << 2); // D2
+  PORTD ^= (-(pwmCounter >= selected_pwms[1]) ^ PORTD) & (1 << 3); // D3
+  PORTD ^= (-(pwmCounter >= selected_pwms[2]) ^ PORTD) & (1 << 4); // D4
+  PORTD ^= (-(pwmCounter >= selected_pwms[3]) ^ PORTD) & (1 << 5); // D5
+  PORTD ^= (-(pwmCounter >= selected_pwms[4]) ^ PORTD) & (1 << 6); // D6
+  PORTD ^= (-(pwmCounter >= selected_pwms[5]) ^ PORTD) & (1 << 7); // D7
+
+  PORTB ^= (-(pwmCounter >= selected_pwms[6]) ^ PORTB) & (1 << 0); // D8
+  PORTB ^= (-(pwmCounter >= selected_pwms[7]) ^ PORTB) & (1 << 1); // D9
 
   pwmCounter += 1;
   
   // Encoder reading logic
-  last_enca = (PINB & (1 << PB3)) != 0;
-  last_encb = (PINB & (1 << PB4)) != 0; 
-  
-  byte enca = (PINB & (1 << PB3)) != 0;
-  byte encb = (PINB & (1 << PB4)) != 0;
-
-  // Compare the current and last states to determine direction
-  if(enca) {
-    if(!last_encb) {
-      counter++; // if encoder A is on and B last state is 0 CLOCKWISE
-    } else {
-      return;
-    }
-  } else if(encb){
-    if(!last_enca) {
-      counter--; // if encoder B is on and A last state is 0 COUNTER-CLOCKWISE
-    } else{
-      return;
-    }
+  unsigned char result = enc1.process_n();
+  if (result == DIR_CW) {
+    counter++;
+  } else if (result == DIR_CCW) {
+    counter--;
   }
-  
-  //Serial.println(counter);
+  Serial.println(counter);
+
 }
